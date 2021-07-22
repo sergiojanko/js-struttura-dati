4a2534b59db977e2a6b034427f4e4d20481e3da3
sergiojanko <janko.sergio1998@gmail.com>
2021-07-22 17:04:24 +0200

    card

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const card = {
+    name: "Bloodfire Colossus",
+    launchCost: ["6","R","R"],
+    manoCost: 8,
+    type: "Creatura",
+    subType: "Giant",
+    expansion: {
+        reprintId: 9, 
+        name: "Espansione", 
+        rarity: "Gold",
+        collectionNr: 12,
+    },
+    flavorText: {
+        quote: "Lorem",
+        author: "Gigi",
+    },
+    abilities: [
+        {
+            launchCost: ["R", "T"],
+            description: "Lorem ipsum...",
+        },
+        {
+            launchCost: ["R", "T", "W"],
+            description: "Lorem ipsum...",
+        },
+    ],
+    illustrator: "Greg Smith",
+    strength: 13,
+    constitution: 13,
+    illustration: {
+        backgroundColor: "red",
+        border: "black",
+        image: "source.png",
+    },
+};
+
+
+
