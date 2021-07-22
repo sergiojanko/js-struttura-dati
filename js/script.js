f2becb5be8919a046495d34ae2e3acd33c06a10d
sergiojanko <janko.sergio1998@gmail.com>
2021-07-22 17:50:10 +0200

    data-output

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const card = {
     name: "Bloodfire Colossus",
     launchCost: ["6","R","R"],
-    manoCost: 8,
+    manaCost: 8,
     type: "Creatura",
     subType: "Giant",
     expansion: {
@@ -35,4 +35,46 @@ const card = {
 };
 
 
+const cardSection = document.getElementById("cards");
 
+let subType = card.subType ? card.subType : ``;
+
+let abilities = `Non ci sono abilità`;
+
+if (card.abilities.length) {
+    abilities = `<ul>`
+    for (let i = 0; i < card.abilities.length; i++){
+        const currentAbility = card.abilities[i];
+        abilities += `<li>Descrizione: ${currentAbility.description}</li>`;
+        abilities += `<li>Costo di Lancio: ${currentAbility.launchCost.join(" ")}</li>`;
+    }
+    abilities += "</ul>"
+}
+
+let cardTemplate = `
+<ul class="card">
+    <li>Nome: ${card.name}</li>
+    <li>Costo di lancio: ${card.launchCost.join(" ")}</li>
+    <li>Costo Mana: ${card.manaCost}</li>
+    <li>Tipo: ${card.type} - ${subType}</li>
+    <li>Espansione:
+        <ul>
+            <li>Ristampa: ${card.expansion.reprintId}</li>
+            <li>Nome: ${card.expansion.name}</li>
+            <li>Rarità: ${card.expansion.rarity}</li>
+            <li>Nr. Collezione: ${card.expansion.collectionNr}</li>
+        </ul>
+    </li>
+    <li>Testo di colore: ${card.flavorText.quote} - ${card.flavorText.author}</li>
+    <li>Abilità: ${abilities}</li>
+    <li>Forza: ${card.strength}</li>
+    <li>Costituzione: ${card.constitution}</li>
+    <li>Artista: ${card.illustrator}</li>
+    <li>Colore Bordo: ${card.illustration.border}</li>
+    <li>Colore Sfondo: ${card.illustration.backgroundColor}</li>
+    <li><img src=" ${card.illustration.image}" alt="Immagine"></li>
+
+</ul>
+`;
+
+cardSection.innerHTML = cardTemplate;
